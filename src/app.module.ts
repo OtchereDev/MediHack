0bd9c65475de8309c19e43d100f16be479b8867f
Oliver Otchere <oliverotchere4@gmail.com>
2024-07-08 14:40:00 +0100

    cleanup setup files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule],
+  controllers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
