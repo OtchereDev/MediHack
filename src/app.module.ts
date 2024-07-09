e3f5221b755410591951778a22afcdec32609a10
Oliver Otchere <oliverotchere4@gmail.com>
2024-07-09 10:30:00 +0100

    added jwt setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma.service';
+import { AuthModule } from './auth/auth.module';
+import { jwtConstants } from './constant/constant';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   controllers: [],
   providers: [PrismaService],
 })
