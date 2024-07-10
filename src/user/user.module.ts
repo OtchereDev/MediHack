710bfeeb2c3489cdc70eb143b182dd56483e9a84
Oliver Otchere <oliverotchere4@gmail.com>
2024-07-11 00:52:53 +0100

    added auth guard and user components

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { PrismaService } from 'src/prisma.service';
+import { MailModule } from 'src/mail/mail.module';
 
 @Module({
-  providers: [UserService],
-  controllers: [UserController]
+  providers: [UserService, PrismaService],
+  controllers: [UserController],
+  exports: [UserService],
+  imports: [MailModule],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,76 @@
 import { Injectable } from '@nestjs/common';
+import { User, ForgotPassword } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import otp from 'src/utils/otp';
+import bcrypt from 'bcrypt';
+import { MailService } from 'src/mail/mail.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    private prismaClient: PrismaService,
+    private mailService: MailService,
+  ) {}
+
+  async findOne(email: string): Promise<User | undefined> {
+    return await this.prismaClient.user.findFirst({ where: { email } });
+  }
+
+  async requestForgotPassword(email: string, name: string) {
+    const code = otp(6, {
+      upperCaseAlphabets: false,
+      lowerCaseAlphabets: false,
+      digits: true,
+      specialChars: false,
+    });
+
+    await this.prismaClient.forgotPassword.create({
+      data: {
+        email,
+        code,
+      },
+    });
+
+    const message = `Hello ${name} \nUse this code to reset your account \nCode: ${code} \nBest regards, \nLaedral Express`;
+
+    await this.mailService.sendMail(message, email, 'Reset password');
+  }
+
+  async findResetPassword(email: string, code: string) {
+    const otp = await this.prismaClient.forgotPassword.findFirst({
+      where: {
+        email,
+        code,
+        isUsed: false,
+      },
+    });
+
+    return otp;
+  }
+
+  async deactivateOtp(otp: ForgotPassword) {
+    await this.prismaClient.forgotPassword.update({
+      where: {
+        id: otp.id,
+      },
+      data: {
+        isUsed: true,
+      },
+    });
+  }
+
+  async updateUserPassword(id: number, password: string) {
+    //   hash user password
+    const saltRounds = 10;
+    const hashPassword = bcrypt.hashSync(password, saltRounds);
+
+    await this.prismaClient.user.update({
+      where: {
+        id,
+      },
+      data: {
+        password: hashPassword,
+      },
+    });
+  }
+}
