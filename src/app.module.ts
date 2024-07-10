fdd1f105ff47df0622f4b5c6615151151f6a8e67
Oliver Otchere <oliverotchere4@gmail.com>
2024-07-11 00:51:32 +0100

    added mail and config mod

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,22 @@ import { PrismaService } from './prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { jwtConstants } from './constant/constant';
 import { JwtModule } from '@nestjs/jwt';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { ConfigModule } from '@nestjs/config';
+import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [
     UserModule,
     AuthModule,
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '1d' },
     }),
+
+    MailModule,
   ],
   controllers: [],
   providers: [PrismaService],
